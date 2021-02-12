13d45f059e89ccbb25bd0170fe3c41acf821f114
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-02-13 00:22:45 +0100

    change some elemnt

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,10 +1,36 @@
+let get_fulltype = document.querySelector(".fulltype");
 
 
+
+function show_fulltype(porcentage)
+{   
+    console.log(porcentage);
+}
 function show_price(price,type,fulltype)
 {
-    var ress = price + price*type;
+    let porc_fulltype = document.querySelector("#por_fulltype");
+    var ress = 0;
+    ress = price + price*type;
     console.log(ress,fulltype);
+
+
+
+    if(fulltype== 'moto')
+    {
+        
+        get_fulltype.innerHTML += ` 
+        <button id="por_fulltype"  onclick="show_fulltype(0.66666)">essense<button>
+        <button id="por_fulltype"   onclick="show_fulltype(0.11111)">mazzote<button>
+        `;
+    }
+    if(fulltype== 'compat')
+    {
+        
+        get_fulltype.innerHTML += ` 
+        <button id="por_fulltype"  onclick="show_fulltype(0.66666)">essense<button>
+        <button id="por_fulltype"   onclick="show_fulltype(0.11111)">mazzote<button>
+        `;
+    }
+
 }
 
-
-// chose_car.onclick= show_price;
