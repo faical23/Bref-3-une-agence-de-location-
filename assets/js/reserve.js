8818ba446dcaad84efae2fa7346760081c3c9636
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-02-13 02:27:19 +0100

    algo calcule

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,6 +1,9 @@
-let get_fulltype = document.querySelector(".fulltype");
+let get_fulltype = document.querySelector(".fulltype"); /* div get_fulltype */
+let jours = document.querySelector(".jours"); /* div jours */
+let btn_valide = document.querySelector(".btn-valide"); /* btn validation */
 
-var test = [' <button id="por_fulltype"  onclick="show_fulltype(0.05)">Elictrique<button>'
+
+var all_fulltype = [' <button id="por_fulltype"  onclick="show_fulltype(0.05)">Elictrique<button>'
             ,'<button id="por_fulltype"   onclick="show_fulltype(0.14)">essense<button></button>'
             ,'<button id="por_fulltype"  onclick="show_fulltype(0.09)">hyprede<button>'
             , '<button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>']
@@ -8,53 +11,53 @@ var test = [' <button id="por_fulltype"  onclick="show_fulltype(0.05)">Elictriqu
 function moto()
 {
     get_fulltype.innerHTML = ` 
-    ${test[0]}
-    ${test[1]}
+    ${all_fulltype[0]}
+    ${all_fulltype[1]}
     `;
 }
 function compat()
 {
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"  onclick="show_fulltype(0.14)">essense<button>
-    <button id="por_fulltype"  onclick="show_fulltype(0.09)">hyprede<button>
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[1]}
+    ${all_fulltype[2]}
+    ${all_fulltype[3]}
     `;
 }
 function Berlin()
 {
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"  onclick="show_fulltype(0.14)">essense<button>
-    <button id="por_fulltype"  onclick="show_fulltype(0.09)">hyprede<button>
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[1]}
+    ${all_fulltype[2]}
+    ${all_fulltype[3]}
     `;
 }
 function Cetadin()
 {
     
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"  onclick="show_fulltype(0.05)">Elictrique<button>
-    <button id="por_fulltype"  onclick="show_fulltype(0.14)">essense<button>
-    <button id="por_fulltype"  onclick="show_fulltype(0.09)">hyprede<button>
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[0]}
+    ${all_fulltype[1]}
+    ${all_fulltype[2]}
+    ${all_fulltype[3]}
     `;
 }
 function Utilitaire()
 {
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[3]}
     `;
 }
 function Camion()
 {
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[3]}
     `;
 }
 function Engin()
 {
     get_fulltype.innerHTML = ` 
-    <button id="por_fulltype"  onclick="show_fulltype(0.14)">essense<button>
-    <button id="por_fulltype"   onclick="show_fulltype(0.21)">diesel<button>
+    ${all_fulltype[1]}
+    ${all_fulltype[3]}
     `;
 }
 
@@ -96,10 +99,24 @@ function show_price(price,type,fulltype)
           // code block
       }
 }
+var h ;
 function show_fulltype(porcentage)
 {   
+    h = porcentage;
+    jours.innerHTML = ` 
+    <h1>how much days</h1>
+    <input type="number" class="jrs">
+    `;
     console.log(porcentage);
     var pp = x+x*y+x*porcentage;
     console.log(pp);
 }
 
+
+
+function validation()
+{
+    let get_jrs = document.querySelector(".jrs").value; /* get number jrs */
+    montant_total = (x+x*y+x*h)*get_jrs;
+    alert(montant_total);
+}
