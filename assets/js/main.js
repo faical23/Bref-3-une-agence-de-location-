ec397613964d6c2ba3067d336036610d39231cfd
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-02-14 04:29:07 +0100

    navbar MOBILE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
-/*  burger clicked */
+/*  burger clicked and removed */
 
 
 var burger_menu = document.querySelector(".burger-menu");
+var navbar_mobile_off = document.querySelector("#nav-bar-mobile_off");
 
 function bruger_clicked()
 {
-    burger_menu.id = "tested";
+    navbar_mobile_off.id="nav-bar-mobile_on";
+}
+
+function bruger_removed(){
+    navbar_mobile_off.id="nav-bar-mobile_off";
 }
